ed3149dc2412604f47a319b54e8af49052dc0965
yangjuna <925413593@qq.com>
2019-06-27 16:36:51 +0800

    Initial commit

---
+++ zhihu.py
@@ -0,0 +1,45 @@
+import requests
+import pyquery
+from pyquery import PyQuery as pq
+
+
+
+
+
+def get_zhihu_page(url,headers):
+    try:
+        page = requests.get(url,headers=headers)
+        if page.status_code == 200:
+            return page.text
+        else:
+            return None
+    except RequestException:
+        return None
+
+def parse_one_page(html):
+    doc =pq(html)
+    items =doc('.explore-tab .explore-feed.feed-item').items()
+
+    for item in items:
+        question = item.find('.question_link').text()
+        write_to_file(question)
+        author = item.find('.author-link-line').text()
+        write_to_file(author)
+        answer = pq(item.find('.content').html()).text()
+        write_to_file(answer)
+
+def write_to_file(result):
+    with open('D:\project/yangjun.txt','a',encoding='utf8')as  f:
+        f.write(result)
+        f.write('\n' + '=' * 50 + '\n')
+
+
+url = 'https://www.zhihu.com/explore'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+}
+
+
+
+html =get_zhihu_page(url,headers)
+parse_one_page(html)
